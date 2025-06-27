e04de3252d56d0eb6d503c167c6ddc6214ad8986
Ayush Srivastava <ayushsrivas55@gmail.com>
2025-06-27 18:12:59 +0530

    deployable

--- zyvilla/lib/queue.ts
+++ zyvilla/lib/queue.ts
@@ -3,6 +3,9 @@ import { Queue } from 'bullmq';
 const connection = {
   host: process.env.REDIS_HOST || 'localhost',
   port: process.env.REDIS_PORT ? parseInt(process.env.REDIS_PORT, 10) : 6379,
+  username: process.env.REDIS_USERNAME || 'default',
+  password: process.env.REDIS_PASSWORD,
+  tls: process.env.REDIS_TLS === 'true' ? {} : undefined,
 };
 
 export const imageGenerationQueue = new Queue('image-generation', {
